2fad843a33d61d4ba9f6ac7a7100bbcc7bb635f0
Thasmika Gokal <tgokal@microsoft.com>
2021-10-24 14:44:22 +1000

    update score.py

--- driver_training/scoring/score.py
+++ driver_training/scoring/score.py
@@ -23,45 +23,24 @@ IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
 ARISING IN ANY WAY OUT OF THE USE OF THE SOFTWARE CODE, EVEN IF ADVISED OF THE
 POSSIBILITY OF SUCH DAMAGE.
 """
+import json
 import numpy
-import joblib
-import os
 from azureml.core.model import Model
-from inference_schema.schema_decorators \
-    import input_schema, output_schema
-from inference_schema.parameter_types.numpy_parameter_type \
-    import NumpyParameterType
+import joblib
 
 
 def init():
-    # load the model from file into a global object
-    global model
-
-    # we assume that we have just one model
-    # AZUREML_MODEL_DIR is an environment variable created during deployment.
-    # It is the path to the model folder
-    # (./azureml-models/$MODEL_NAME/$VERSION)
+    global LGBM_MODEL
+    # Load the model from file into a global object
     model_path = Model.get_model_path(
-        os.getenv("AZUREML_MODEL_DIR").split('/')[-2])
-
-    model = joblib.load(model_path)
-
-
-input_sample = numpy.array([
-    [1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0, 10.0],
-    [10.0, 9.0, 8.0, 7.0, 6.0, 5.0, 4.0, 3.0, 2.0, 1.0]])
-output_sample = numpy.array([
-    5021.509689995557,
-    3693.645386402646])
+        model_name="driver_model.pkl")
+    LGBM_MODEL = joblib.load(model_path)
 
 
-# Inference_schema generates a schema for your web service
-# It then creates an OpenAPI (Swagger) specification for the web service
-# at http://<scoring_base_url>/swagger.json
-@input_schema('data', NumpyParameterType(input_sample))
-@output_schema(NumpyParameterType(output_sample))
-def run(data, request_headers):
-    result = model.predict(data)
+def run(raw_data, request_headers):
+    data = json.loads(raw_data)["data"]
+    data = numpy.array(data)
+    result = LGBM_MODEL.predict(data)
 
     # Demonstrate how we can log custom data into the Application Insights
     # traces collection.
@@ -85,6 +64,6 @@ def run(data, request_headers):
 if __name__ == "__main__":
     # Test scoring
     init()
-    test_row = '{"data":[[1,2,3,4,5,6,7,8,9,10],[10,9,8,7,6,5,4,3,2,1]]}'
-    prediction = run(test_row, {})
-    print("Test result: ", prediction)
+    TEST_ROW = '{"data":[[0,1,8,1,0,0,1,0,0,0,0,0,0,0,12,1,0,0,0.5,0.3,0.610327781,7,1,-1,0,-1,1,1,1,2,1,65,1,0.316227766,0.669556409,0.352136337,3.464101615,0.1,0.8,0.6,1,1,6,3,6,2,9,1,1,1,12,0,1,1,0,0,1],[4,2,5,1,0,0,0,0,1,0,0,0,0,0,5,1,0,0,0.9,0.5,0.771362431,4,1,-1,0,0,11,1,1,0,1,103,1,0.316227766,0.60632002,0.358329457,2.828427125,0.4,0.5,0.4,3,3,8,4,10,2,7,2,0,3,10,0,0,1,1,0,1]]}'  # NOQA: E501
+    PREDICTION = run(TEST_ROW, {})
+    print("Test result: ", PREDICTION)
