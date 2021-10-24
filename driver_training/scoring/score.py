a0ec1c8b1ef47db48d9c5b04e03961c141557242
Thasmika Gokal <tgokal@microsoft.com>
2021-10-24 16:57:42 +1000

    update model_name to pkl file name in Azure ML

--- driver_training/scoring/score.py
+++ driver_training/scoring/score.py
@@ -33,7 +33,7 @@ def init():
     global LGBM_MODEL
     # Load the model from file into a global object
     model_path = Model.get_model_path(
-        model_name="driver_model.pkl")
+        model_name="driver_training_model.pkl")
     LGBM_MODEL = joblib.load(model_path)
 
 
